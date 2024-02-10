d507e70cf959446a20ccd6c8dd555fdb2ad90068
shangsong <1991995430@qq.com>
2024-02-10 19:12:09 +0800

    AA

--- ss-service/ss-practice/src/main/java/com/ss/song/test/Ab.java
+++
@@ -1,7 +0,0 @@
-package com.ss.song.test;
-
-/**
- * author shangsong 2023/12/11
- */
-public class Ab {
-}
--- ss-service/ss-practice/src/main/java/com/ss/song/test/Test1211.java
+++ ss-service/ss-practice/src/main/java/com/ss/song/test/Test1211.java
@@ -8,46 +8,13 @@ import org.checkerframework.checker.units.qual.A;
 public class Test1211 {
 
     public static void main(String[] args) {
-
-        Ab ab = new Ab();
-
-        Thread t1 = new Thread(() -> {
-            System.out.println("线程：" + Thread.currentThread().getName() + "开始");
-            synchronized (ab) {
-                try {
-                    ab.wait(5000);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("我是新线程");
+                System.out.println(Thread.currentThread().getName());
             }
-            System.out.println("线程：" + Thread.currentThread().getName() + "结束");
-        });
-
-        Thread t2 = new Thread(() -> {
-            System.out.println("线程：" + Thread.currentThread().getName() + "开始");
-            try {
-                Thread.sleep(3000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            System.out.println("线程：" + Thread.currentThread().getName() + "结束");
-        });
-
-        Thread t3 = new Thread(() -> {
-            System.out.println("线程：" + Thread.currentThread().getName() + "开始");
-            /*synchronized (ab) {
-                ab.notify();
-            }*/
-            try {
-                Thread.sleep(1000); // 添加延迟，给予 t1 获取锁并继续执行的机会
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            System.out.println("线程：" + Thread.currentThread().getName() + "结束");
         });
         t1.start();
-        t2.start();
-        t3.start();
     }
-
 }
